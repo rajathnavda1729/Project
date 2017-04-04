96ce480c75fecd04ef6b753877cf49aab4da32ce
Rohit Goudar <rohitgoudar444@gmail.com>
2017-04-04 17:33:23 +0530

    splash

---
+++ app/src/main/java/com/example/user/project/Splash.java
@@ -0,0 +1,26 @@
+package com.example.user.project;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        int secondsDelayed = 1;
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(Splash.this, TorS.class));
+                finish();
+            }
+        }, secondsDelayed * 3000);
+    }
+
+}
